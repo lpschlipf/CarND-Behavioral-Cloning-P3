966b47657567f1fb6d19e832ea7806a10ae3f745
lpschlipf <lpschlipf@gmail.com>
2020-04-27 11:25:20 +0000

    Adding data processing methods and a trained model that sucessfully drives a lap.

--- model.py
+++ model.py
@@ -9,13 +9,17 @@ import matplotlib.pyplot as plt
 import csv
 import os
 from scipy import ndimage
+import cv2
 from keras.applications.mobilenetv2 import mobilenet_v2
 from keras.models import Sequential
-from keras.layers import Dense, Flatten, Dropout, Conv2D, AveragePooling2D
+from keras.layers import Dense, Flatten, Dropout, Conv2D, AveragePooling2D, Lambda
 
 
 def create_model_architecture(input_shape=(160, 320, 3)):
     model = Sequential()
+    ### Data Preprocessing ###
+    # Norming Pixels and mean centering
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
     # Convolutional Layer 1
     model.add(Conv2D(filters=6, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
     model.add(AveragePooling2D())
@@ -52,15 +56,23 @@ if __name__ == '__main__':
         # Careful with the path, it was recorded in a workspace under unix, check where you are!
         img_name = os.path.split(line[0])[-1]
         images.append(ndimage.imread(os.path.join('data_regular/IMG/', img_name)))
+        
+    # Also create flipped images with negated steering angle to gain more data
+    augmented_images, augmented_angles = [], []
+    for image, angle in zip(images, angles):
+        augmented_images.append(image)
+        augmented_angles.append(angle)
+        augmented_images.append(cv2.flip(image, 1))
+        augmented_angles.append(-1.0 * angle)
 
     # Finalize training data
-    X_train = np.array(images)
-    y_train = np.array(angles)
+    X_train = np.array(augmented_images)
+    y_train = np.array(augmented_angles)
 
     # Create model architecture
     model = create_model_architecture()
 
     # Stick it in the optimizer and train it
     model.compile(loss='mse', optimizer='adam')
-    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=10)
+    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=3)
     model.save('model.h5')
